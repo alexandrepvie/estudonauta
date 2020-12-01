4001ce394f57a608b808a3473f5410f7d6995fd3
Alexandre Pereira Vieira <alexandrepvie@gmail.com>
2020-12-01 03:06:20 -0300

    aula m01a10
    aprimorando o filtro de buscas, agora filtra tanto a string como as pré seleções de filtro

--- index.php
+++ index.php
@@ -22,10 +22,10 @@
         <h1>Escolha seu jogo</h1>
         <form id="busca" action="index.php" method="get">
             Ordenar: 
-             <a href="index.php?o=n">Nome</a> |
-             <a href="index.php?o=p">Produtora</a> |
-             <a href="index.php?o=n1">Nota Alta</a> |
-             <a href="index.php?o=n2">Nota Baixa</a> |
+             <a href="index.php?o=n&c=<?php echo $chave;?>">Nome</a> |
+             <a href="index.php?o=p&c=<?php echo $chave;?>">Produtora</a> |
+             <a href="index.php?o=n1&c=<?php echo $chave;?>">Nota Alta</a> |
+             <a href="index.php?o=n2&c=<?php echo $chave;?>">Nota Baixa</a> |
              <a href="index.php">Mostrar Todos</a> |
             Buscar: <input type="text" name="c" size="10" maxlength="40">
             <input type="submit" value="Ok">
