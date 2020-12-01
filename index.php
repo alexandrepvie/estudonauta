ba2fea1400878a351d3d0d44d753faaee2d02cb4
Alexandre Pereira Vieira <alexandrepvie@gmail.com>
2020-12-01 02:27:13 -0300

    aula m1a09
    fazendo a busca por string funcionar

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
     <?php
         require_once"includes/banco.php";
         require_once"includes/funcoes.php";
-        $ordem = $_GET['o']??"n";
+        $ordem = $_GET['o'] ?? "n";
+        $chave = $_GET['c'] ?? "";
     ?>
     <div id="corpo">
         <?php include_once"topo.php";?>
@@ -25,12 +26,16 @@
              <a href="index.php?o=p">Produtora</a> |
              <a href="index.php?o=n1">Nota Alta</a> |
              <a href="index.php?o=n2">Nota Baixa</a> |
+             <a href="index.php">Mostrar Todos</a> |
             Buscar: <input type="text" name="c" size="10" maxlength="40">
             <input type="submit" value="Ok">
         </form>
         <table class="listagem">
             <?php
                 $q = "select j.cod, j.nome, g.genero, p.produtora, j.capa from jogos j join generos g on j.genero = g.cod join produtoras p on j.produtora = p.cod ";
+                if (!empty($chave)) {
+                    $q .="WHERE j.nome like '%$chave%' OR p.produtora like '%$chave%' OR g.genero like '%$chave%' ";
+                }
                 switch ($ordem) {
                     case 'p':
                         $q .= "ORDER BY p.produtora";
