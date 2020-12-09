2f429d18850de761c8f8b754ddb4ebc4ea97f13c
Alexandre Pereira Vieira <alexandrepvie@gmail.com>
2020-12-09 03:36:46 -0300

    Aula m02a13
    formulário de edição de usuário

--- user-edit.php
+++ user-edit.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <title>???</title>
+    <title>Edição de Dados do Usuário</title>
 </head>
 <body>
 <?php
@@ -14,7 +14,22 @@
     require_once"includes/funcoes.php";
 ?>
     <div id="corpo">
-    
+        <?php
+            if (!is_logado()){
+
+                echo msg_erro("Efetue <a href = 'user-login.php'>login</a> para poder editar seus dados.'");
+
+            } else {
+                if(!isset($_POST['usuario'])) {
+
+                    include "user-edit-form.php";
+
+                } else {
+
+                    echo msg_sucesso("Dados foram passados");
+                }
+            }
+        ?>
     </div>
     <?php require_once "rodape.php";?>
 </body>
